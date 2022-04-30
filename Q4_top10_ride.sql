e5c488200dc1c83455ddb8fdf117ac5e7d2d68e9
Wei Wang <weiwang9807@outlook.com>
2022-04-30 13:08:50 -0400

    Final edit
    Finish everything!!! Done!

--- Q4_top10_ride.sql
+++ Q4_top10_ride.sql
@@ -1,6 +1,22 @@
 
-select cast(strftime('%w', pickup_datetime) as int) as day_of_week, count(*) as trip_count
-from uber_trips
-where strftime('%Y-%m',pickup_datetime) between "2009-01" and "2015-06"
-group by strftime('%w', pickup_datetime)
-order by trip_count desc
+select date, ride_count, avg_distance
+from
+(
+    select 
+        strftime('%Y-%m-%d',pickup_datetime) as date,
+        count(trip_distance) as ride_count,
+        avg(trip_distance) as avg_distance
+    from
+    (
+        select pickup_datetime, trip_distance
+        from uber_trips
+        where strftime('%Y',pickup_datetime) = '2009'
+        union all
+        select pickup_datetime, trip_distance
+        from taxi_trips
+        where strftime('%Y',pickup_datetime) = '2009'
+    )
+    group by strftime('%Y-%m-%d',pickup_datetime) 
+)
+order by ride_count desc, avg_distance desc
+limit 10
